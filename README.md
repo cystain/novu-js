b3a2f2ba629365a269f38f0a364113b29c7f061a
Christopher Mendoza <chris@chocsproc.com>
2023-09-18 22:14:54 +0800

    add subscribers.create() method; bump to v0.0.3

--- README.md
+++ README.md
@@ -9,3 +9,36 @@ This aims to provide wider compatibility to other non-node JS runtime.
 #### Use case(s)
 
 - Use on cloudflare workers - Since cloudflare workers do not natively run on Node runtime, `@novu/node` is not usable, mostly due to its use of `axios`. To solve this, we use `fetch()` for performing requests.
+
+## Install as dependency
+
+```shell
+npm i @cystain/novu-rest-js
+```
+
+## Usage
+
+```js
+import { NovuRestClient } from '@cystain/novu-rest-js/lib/restClient';
+
+const novuRestClient = new NovuRestClient('<YOUR_NOVU_API_KEY>');
+
+// Sample trigger request (Trigger Event)
+await novuRestClient.trigger('<WORKFLOW_TRIGGER_ID>', {
+  to: {
+    subscriberId: '<SUBSCRIBER_ID>',
+  },
+  payload: {
+    title: 'Notif title',
+    body: 'Notif body',
+    link: 'http://example.com',
+  },
+  overrides: {
+    fcm: {
+      type: 'data',
+    },
+  },
+});
+```
+
+For the request specifications, refer to the REST API doc: https://docs.novu.co/api-reference/events/trigger-event
--- lib/restClient.ts
+++ lib/restClient.ts
@@ -1,4 +1,4 @@
-import { ITriggerPayloadOptions } from '@novu/node';
+import { ITriggerPayloadOptions, ISubscriberPayload } from '@novu/node';
 
 /**
  * The Novu Rest API Client
@@ -10,7 +10,10 @@ export class NovuRestClient {
   /**
    * @param apiKey the API Key of the Novu account
    */
-  constructor(apiKey: string) {
+  constructor(apiKey: string | undefined) {
+    if (!apiKey) {
+      throw new Error(`Invalid API Key: ${apiKey}`);
+    }
     this.API_KEY = apiKey;
   }
 
@@ -33,4 +36,27 @@ export class NovuRestClient {
       }),
     });
   }
+
+  subscribers = {
+    /**
+     * Creates a new subscriber. This has an upsert behavior, only updating attributes that are present in the request.
+     * (REST API doc: https://docs.novu.co/api-reference/subscribers/create-subscriber)
+     * @param subscriberId
+     * @param subscriberPayloadOpts
+     * @returns
+     */
+    create: async (subscriberId: string, subscriberPayloadOpts: ISubscriberPayload) => {
+      return fetch(`${this.API_BASE_URL}/subscribers`, {
+        method: 'POST',
+        headers: {
+          Authorization: `ApiKey ${this.API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          subscriberId,
+          ...subscriberPayloadOpts,
+        }),
+      });
+    },
+  };
 }
