fe6b6d1599db27f4501fc1eee70c8162527f9f41
Christopher Mendoza <chris@chocsproc.com>
2023-09-18 14:56:25 +0800

    initial NovuRestClient w/ trigger function

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# novu-js
-Novu Rest API client for JS
+# Novu Rest Client for JS
+
+A third-party library for invoking the REST APIs of Novu
+
+### Motivation
+
+This aims to provide wider compatibility to other non-node JS runtime.
+
+#### Use case(s)
+
+- Use on cloudflare workers - Since cloudflare workers do not natively run on Node runtime, `@novu/node` is not usable, mostly due to its use of `axios`. To solve this, we use `fetch()` for performing requests.
---
+++ src/lib/restClient.ts
@@ -0,0 +1,36 @@
+import { ITriggerPayloadOptions } from '@novu/node';
+
+/**
+ * The Novu Rest API Client
+ */
+export class NovuRestClient {
+  private API_KEY: string | null = null;
+  private API_BASE_URL: string = 'https://api.novu.co/v1';
+
+  /**
+   * @param apiKey the API Key of the Novu account
+   */
+  constructor(apiKey: string) {
+    this.API_KEY = apiKey;
+  }
+
+  /**
+   * Trigger a workflow
+   * @param eventName
+   * @param requestBody
+   * @returns
+   */
+  async trigger(eventName: string, triggerPayloadOpts: ITriggerPayloadOptions) {
+    return fetch(`${this.API_BASE_URL}/events/trigger`, {
+      method: 'POST',
+      headers: {
+        Authorization: `ApiKey ${this.API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: eventName,
+        triggerPayloadOpts,
+      }),
+    });
+  }
+}
