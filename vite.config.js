1e94a6ac8d9496fd6e020ee935669780d1481dd1
Christopher Mendoza <chris@chocsproc.com>
2023-09-22 18:04:11 +0800

    implement distribution transpiling/bundling (to CommonJS) using Vite

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import pkg from './package.json' assert { type: 'json' };
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: {
+        restClient: resolve(__dirname, 'src/restClient.ts'),
+      },
+      formats: ['cjs'],
+      name: 'NovuRestJS',
+      fileName: (format, name) => {
+        if (format === 'es') {
+          return `${name}.js`;
+        }
+        return `${name}.${format}`;
+      },
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies), // don't bundle dependencies
+        /^node:.*/, // don't bundle built-in Node.js modules (use protocol imports!)
+      ],
+    },
+    target: 'es6', // transpile as little as possible
+  },
+  plugins: [dts()], // emit TS declaration files
+});
